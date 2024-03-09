82a74a101f7bcbd71091e535b818289128520ab3
HARSH NISHAD <91336535+harshnishad@users.noreply.github.com>
2024-03-09 10:51:21 +0530

    hii

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var { graphql, buildSchema } = require("graphql")
-
+var express  = require ("express");
+const app=express();
+var {createHandler} = require('graphql-http/lib/use/express')
+var {ruruHTML} = require('ruru/server')
+// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
@@ -7,13 +11,27 @@ var schema = buildSchema(`
   }
 `)
 
-var rootValue = { 
-    hello: () => { return "Hello world!"},
-     age: ()=>{return 25}
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return "Hello world!"
+  },
+  age: () => {
+    return 20
+  },
 }
 
-var source = "{ age }"
 
-graphql({ schema, source, rootValue }).then(response => {
-  console.log(response)
-})
+app.all(
+    "/graphql",
+    createHandler({
+      schema: schema,
+      rootValue: root,
+    })
+  )
+//help to intract with server
+app.get("/", (_req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+  })
+app.listen(4000);
